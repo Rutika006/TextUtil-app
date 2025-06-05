3615e6684fe7d9f48ffc248f71be9a56e970c913
Rutika Murlidhar Bagul <rutikabagul06@gmail.com>
2025-06-05 21:33:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ import BrowserRouter
 import App from "./App";
-createRoot(document.getElementById("root")).render(<App/>)
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/TextUtil-app">  {/* ✅ this is key for GitHub Pages */}
+    <App />
+  </BrowserRouter>
+);
