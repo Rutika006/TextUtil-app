36dd014576fd45405afbef2d4c3114ed31965961
Rutika Murlidhar Bagul <rutikabagul06@gmail.com>
2025-06-06 14:51:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";  // Import HashRouter
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename="/TextUtil-app"> {/* Wrap your App with HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
