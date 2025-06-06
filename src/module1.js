9cc3c4c893127d338f713fa1eb2fdad13992a690
Rutika <rutikabagul06@gmail.com>
2025-06-06 14:37:45 +0530

    Deleted module1.js and module2.js

--- src/module1.js
+++
@@ -1,9 +0,0 @@
-import mo,{b,c,d} from './module2.js';
-console.log(mo);
-console.log(b);
-console.log(c);
-console.log(d);
-
-
-
-
--- src/module2.js
+++
@@ -1,8 +0,0 @@
-const a="one";
-const b="two";
-const c="three";
-const d="four";
-export default a;
-export {b};
-export {c};
-export {d};
