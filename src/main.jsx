924fa79dcbec59ff537eb4710bf2a696ff7eab31
Rutika Murlidhar Bagul <rutikabagul06@gmail.com>
2025-06-06 14:19:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ import BrowserRouter
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/TextUtil-app">  {/* ✅ this is key for GitHub Pages */}
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
